bbd726da575e610f657a0e5e9e8d66df935bb616
Gilbert Łaskawiec <gilbertlaskawiec@gmail.com>
2019-02-10 16:41:12 +0100

    Experimental version

---
+++ index.js
@@ -0,0 +1,24 @@
+import React, { createContext, useContext, useReducer } from 'react';
+
+const Context = createContext();
+
+export const Provider = ({ store, children }) => (
+    <Context.Provider value={useReducer(store, store())}>
+        {children}
+    </Context.Provider>
+)
+
+export const createStoreWithReducers = (schema) => {
+    return (state, action) => {
+        let combinedReducers = {};
+        const schemaEntries = Object.entries(schema);
+        for (let i of schemaEntries) {
+            const namespace = i[0];
+            const reducerFcn = i[1];
+            combinedReducers[namespace] = reducerFcn(state && state[namespace], action);
+        }
+        return combinedReducers;
+    }
+}
+
+export const useHoux = () => useContext(Context);
