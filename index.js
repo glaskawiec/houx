9ec096ce8ebeb43bf8a8afdbc1b456ee83f9e462
Gilbert Łaskawiec <gilbertlaskawiec@gmail.com>
2019-02-10 16:55:15 +0100

    Dependency fixes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useReducer } from 'react';
 
-const Context = createContext();
+export const Context = createContext();
 
 export const Provider = ({ store, children }) => (
     <Context.Provider value={useReducer(store, store())}>
